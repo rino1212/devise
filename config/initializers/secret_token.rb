5979e4f8c1d437a697f63231e722950758bae2bd
rino <uenohara524@gmail.com>
2014-12-23 09:09:42 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devise::Application.config.secret_key_base = 'a3ad18aa176cf60facd67bf914acbbc098eac6aacbec3a4d2262e2561d5542357f131c0c2309d873f333c043c1e99ab9e65b1db5d8eeca995e744976b71928ea'
