e7d0c62bac768c191183aff09d7f15057f1e5735
rino <uenohara524@gmail.com>
2014-12-24 10:55:20 +0900

    名前とアドレス両ログイン許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
-      # sign_inのときに、usernameも許可する
-      devise_parameter_sanitizer.for(:sign_in) << :username
-      # sign_upのときに、usernameも許可する
-      devise_parameter_sanitizer.for(:sign_up) << :username
-      #  account_updateのときに、usernameも許可する
-      devise_parameter_sanitizer.for(:account_update) << :username
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  validates :username, presence: true, uniqueness: true
+         :recoverable, :rememberable, :trackable, :validatable,:authentication_keys => [:login]
+
+  validates :username, presence: true, :uniqueness => {
+    :case_sensitive => false
+  }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
+
